d4548771377f54b9f4be1f8248234070e90c5ade
fernanduandrade <onanduandrade@gmail.com>
2023-09-30 12:35:14 -0300

    intial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react'
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Jah pode beber?',
+  description: 'Descubra se está liberado beber sim ou não.',
+  keywords: 'tá liberado beber, beber, tomar uma, beber uma cerveja, já pode beber?', 
+  authors: { name: 'He4rt Devs', url: 'https://heartdevs.com/' },
+  openGraph: {
+    title: 'Jah pode beber?',
+    description: 'Descubra se você já pode al mossar sim ou não.',
+    type: 'website',
+    images: [
+      { url: 'beer.png', width: 1200, height: 630, alt: 'cerveja img' },
+    ],
+  },
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" className="!scroll-smooth">
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
+    </html>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image'
+import Zequinha from '../../public/zequinha.png'
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <div>
+        <h1 className='text-white text-2xl'>Jah Pode beber?</h1>
+      </div>
+      <h2 className="text-white">SIM!!</h2>
+      <Image
+      src={Zequinha}
+        alt="Zeca pagodinho"
+        width="800"
+        height="800"
+        quality="95"
+        priority={true}
+        className="h-24  w-24 rounded-full object-cover border-[0.35rem] border-[#f3f3f3] shadow-xl"
+        />
+    </main>
+  )
+}
