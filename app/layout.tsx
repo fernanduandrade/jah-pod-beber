1452eac7e51d69257c7f0468b9d9ae9ff9022fca
Fernando Andrade <luiz.abreu@agrotools.com.br>
2023-10-06 06:47:19 -0300

    fix: title metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,12 +6,12 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Jah pode beber?',
-  description: 'Será que já chegou a hora de al-mossar? Descubra',
+  description: 'Será que já chegou a hora de beber? Descubra',
   keywords: 'tá liberado beber, beber, tomar uma, beber uma cerveja, já pode beber?, bora tomar uma?, tá liberado?', 
   authors: { name: 'He4rt Devs', url: 'https://heartdevs.com/' },
   openGraph: {
     title: 'Jah pode beber?',
-    description: 'Será que já chegou a hora de al-mossar? Descubra',
+    description: 'Será que já chegou a hora de beber? Descubra',
     type: 'website',
     images: [
       { url: 'beer.png', width: 1200, height: 630, alt: 'cerveja img' },
