c6409e5e0df141f9f531b91ad9bcfd3c196b16fa
fernanduandrade <onanduandrade@gmail.com>
2024-12-07 11:09:03 -0300

    fix:ajuste glowing text

--- app/components/GlowingArrow/index.tsx
+++ app/components/GlowingArrow/index.tsx
@@ -7,9 +7,9 @@ type GlowingArrowProps = {
 export const GlowingArrow = (props: GlowingArrowProps) => {
     return (
         <div className={`wrapper ${props.invert ? 'rotate-180' : ''}`}>
-            <i className="zmdi zmdi-chevron-right"></i>
-            <i className="zmdi zmdi-chevron-right"></i>
-            <i className="zmdi zmdi-chevron-right"></i>
+            <i className="sm:text-[10px] md:text-[20[px] lg:text-[40px] sm:ml-2 md:ml-4 lg:ml-6 zmdi zmdi-chevron-right"></i>
+            <i className="sm:text-[10px] md:text-[20[px] lg:text-[40px] sm:ml-2 md:ml-4 lg:ml-6 zmdi zmdi-chevron-right"></i>
+            <i className="sm:text-[10px] md:text-[20[px] lg:text-[40px] sm:ml-2 md:ml-4 lg:ml-6 zmdi zmdi-chevron-right"></i>
         </div>
     )
 }
